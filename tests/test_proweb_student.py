86966d3a5fb8ef4507e36a9542171c1f17989e0f
Iskandarius <iskandarius.5@gmail.com>
2026-02-18 17:55:56 +0500

    proweb student

---
+++ tests/test_proweb_student.py
@@ -0,0 +1,180 @@
+import  time
+from time import sleep
+from selenium.common.exceptions import NoSuchElementException
+import allure
+
+
+from pages.auth_page import AuthPage
+from pages.home_page import HomePage
+from pages.lessons_page import LessonsPage
+
+
+def test_auth_chrome(driver_chrome):
+    driver_chrome.get('https://my.proweb.uz/log-in?q=/home')
+    auth_page = AuthPage(driver_chrome)
+    auth_page.enter_login('my@proweb.uz')
+    time.sleep(2)
+    auth_page.click_btn_login()
+    time.sleep(2)
+    auth_page.enter_password('proweb3106632')
+    time.sleep(2)
+    auth_page.click_btn_password()
+    time.sleep(2)
+    try:
+        auth_page.click_btn_session()
+        time.sleep(2)
+        auth_page.click_btn_finish()
+    except NoSuchElementException:
+        pass
+
+
+    home_page = HomePage(driver_chrome)
+    home_page.click_video_instruction()
+    time.sleep(2)
+    home_page.click_fullscreen_btn()
+    time.sleep(10)
+    home_page.click_pause()
+    time.sleep(2)
+    home_page.click_fullscreen_exit()
+    time.sleep(2)
+    home_page.click_main_btn()
+    time.sleep(2)
+    home_page.click_group_card()
+    time.sleep(2)
+
+    lessons_page = LessonsPage(driver_chrome)
+    lessons_page.click_lessons_btn()
+    time.sleep(2)
+    lessons_page.click_last_lesson()
+    time.sleep(2)
+    lessons_page.click_fullscreen_btn()
+    time.sleep(2)
+    lessons_page.click_play_btn()
+    time.sleep(10)
+    lessons_page.click_fullscreen_exit()
+    time.sleep(2)
+
+    home_page.click_profile_icon()
+    time.sleep(2)
+    home_page.click_exit_btn()
+    time.sleep(2)
+    home_page.click_confirm_exit()
+
+
+def test_invalid_auth_chrome(driver_chrome):
+    driver_chrome.get('https://my.proweb.uz/log-in?q=/home')
+    auth_page = AuthPage(driver_chrome)
+    auth_page.enter_login('my@proweb.uz')
+    time.sleep(2)
+    auth_page.click_btn_login()
+    time.sleep(2)
+    auth_page.enter_password('proweb4703818')
+    time.sleep(2)
+    auth_page.click_btn_password()
+    time.sleep(2)
+
+
+def test_auth_firefox(driver_firefox):
+    driver_firefox.get('https://my.proweb.uz/log-in?q=/home')
+    auth_page = AuthPage(driver_firefox)
+    auth_page.enter_login('my@proweb.uz')
+    time.sleep(2)
+    auth_page.click_btn_login()
+    time.sleep(2)
+    auth_page.enter_password('proweb3106632')
+    time.sleep(2)
+    auth_page.click_btn_password()
+    time.sleep(2)
+    try:
+        auth_page.click_btn_session()
+        time.sleep(2)
+        auth_page.click_btn_finish()
+    except NoSuchElementException:
+        pass
+
+
+    home_page = HomePage(driver_firefox)
+    home_page.click_video_instruction()
+    time.sleep(2)
+    home_page.click_fullscreen_btn()
+    time.sleep(10)
+    home_page.click_pause()
+    time.sleep(2)
+    home_page.click_fullscreen_exit()
+    time.sleep(2)
+    home_page.click_main_btn()
+    time.sleep(2)
+    home_page.click_group_card()
+    time.sleep(2)
+
+    lessons_page = LessonsPage(driver_firefox)
+    lessons_page.click_lessons_btn()
+    time.sleep(2)
+    lessons_page.click_last_lesson()
+    time.sleep(2)
+    lessons_page.click_fullscreen_btn()
+    time.sleep(2)
+    lessons_page.click_play_btn()
+    time.sleep(10)
+    lessons_page.click_fullscreen_exit()
+    time.sleep(2)
+
+    home_page.click_profile_icon()
+    time.sleep(2)
+    home_page.click_exit_btn()
+    time.sleep(2)
+    home_page.click_confirm_exit()
+
+
+
+def test_auth_edge(driver_edge):
+    driver_edge.get('https://my.proweb.uz/log-in?q=/home')
+    auth_page = AuthPage(driver_edge)
+    auth_page.enter_login('my@proweb.uz')
+    time.sleep(2)
+    auth_page.click_btn_login()
+    time.sleep(2)
+    auth_page.enter_password('proweb3106632')
+    time.sleep(2)
+    auth_page.click_btn_password()
+    time.sleep(2)
+    try:
+        auth_page.click_btn_session()
+        time.sleep(2)
+        auth_page.click_btn_finish()
+    except NoSuchElementException:
+        pass
+
+
+    home_page = HomePage(driver_edge)
+    home_page.click_video_instruction()
+    time.sleep(2)
+    home_page.click_fullscreen_btn()
+    time.sleep(10)
+    home_page.click_pause()
+    time.sleep(2)
+    home_page.click_fullscreen_exit()
+    time.sleep(2)
+    home_page.click_main_btn()
+    time.sleep(2)
+    home_page.click_group_card()
+    time.sleep(2)
+
+    lessons_page = LessonsPage(driver_edge)
+    lessons_page.click_lessons_btn()
+    time.sleep(2)
+    lessons_page.click_last_lesson()
+    time.sleep(2)
+    lessons_page.click_fullscreen_btn()
+    time.sleep(2)
+    lessons_page.click_play_btn()
+    time.sleep(10)
+    lessons_page.click_fullscreen_exit()
+    time.sleep(2)
+
+    home_page.click_profile_icon()
+    time.sleep(2)
+    home_page.click_exit_btn()
+    time.sleep(2)
+    home_page.click_confirm_exit()
+
